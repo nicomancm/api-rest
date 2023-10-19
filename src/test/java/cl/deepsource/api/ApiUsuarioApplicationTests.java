902d3bf853a8a7b0aa9b39a95908f02952f1d2a0
Daniel Avila <daigo.tnt@gmail.com>
2023-10-19 16:44:44 -0300

    commit 1

---
+++ src/test/java/cl/deepsource/api/ApiUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.deepsource.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
